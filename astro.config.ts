8cce378f404d251dc068c0b2a27b6635770a0890
iwbc <1209760+iwbc@users.noreply.github.com>
2025-06-16 18:34:47 +0900

    assets系のディレクトリ構成変更

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   integrations: [
     icon({
-      iconDir: 'src/shared/assets/icons',
+      iconDir: 'src/app/icons',
       svgoOptions: {
         plugins: [
           'preset-default',
