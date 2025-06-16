aee12accf0fd7fff2c730b5dfd4d04ced69bc46c
iwbc <1209760+iwbc@users.noreply.github.com>
2025-06-16 13:45:12 +0900

    rename files

--- src/shared/ui/image/index.ts
+++ src/shared/ui/image/index.ts
@@ -1,4 +1,4 @@
 export { default as Image } from './Image.astro';
 export { default as ImagePicture } from './ImagePicture.astro';
 export { default as ImageDefineVars } from './ImageDefineVars.astro';
-export { getImageMetadata } from './common';
+export { getImageMetadata } from './utils';
