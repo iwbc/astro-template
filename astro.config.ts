50c2e858210cb5b59f7b11d39b55066ab9458f0a
iwbc <1209760+iwbc@users.noreply.github.com>
2026-06-11 04:30:37 +0900

    ビルドアセットの出力と出力先を整理

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import htmlBeautifier from 'astro-html-beautifier';
 import icon from 'astro-icon';
 
+const OUTPUT_ASSETS_DIR = 'assets';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com/',
@@ -34,12 +36,48 @@ export default defineConfig({
   compressHTML: false,
   scopedStyleStrategy: 'class',
   build: {
-    assets: 'assets',
+    assets: OUTPUT_ASSETS_DIR,
     inlineStylesheets: 'never',
   },
   vite: {
     build: {
       assetsInlineLimit: 0,
+      cssCodeSplit: false,
+    },
+    environments: {
+      prerender: {
+        build: {
+          rollupOptions: {
+            output: {
+              assetFileNames: (assetInfo) => {
+                if (/\.(png|jpe?g|gif|svg|webp|avif)$/.test(assetInfo.names[0] ?? '')) {
+                  return `${OUTPUT_ASSETS_DIR}/images/[name].[hash][extname]`;
+                }
+                if (/\.css$/.test(assetInfo.names[0] ?? '')) {
+                  return `${OUTPUT_ASSETS_DIR}/styles/[name].[hash][extname]`;
+                }
+                return `${OUTPUT_ASSETS_DIR}/[name].[hash][extname]`;
+              },
+            },
+          },
+        },
+      },
+      client: {
+        build: {
+          rollupOptions: {
+            output: {
+              chunkFileNames: `${OUTPUT_ASSETS_DIR}/scripts/[name].[hash].js`,
+              entryFileNames: `${OUTPUT_ASSETS_DIR}/scripts/[name].[hash].js`,
+              assetFileNames: (assetInfo) => {
+                if (/\.css$/.test(assetInfo.names[0] ?? '')) {
+                  return `${OUTPUT_ASSETS_DIR}/styles/[name].[hash][extname]`;
+                }
+                return `${OUTPUT_ASSETS_DIR}/[name].[hash][extname]`;
+              },
+            },
+          },
+        },
+      },
     },
     css: {
       preprocessorOptions: {
